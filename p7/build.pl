afe0fb4ae28bb6848dfd159caa6ab29b8754786d
Akvelog <sceneviper@hotmail.com>
2016-01-13 16:48:40 +0900

    Version update.

--- p7/build.pl
+++ p7/build.pl
@@ -9,7 +9,7 @@ use Servbuild::Makemaker::C;
 %Servbuild::Makemaker::C::assignments_overwritten = (
     CFLAGS => '-fPIC -c -O2 -g',
     LDFLAGS => '-shared --version-script p7_version_script -lpthread',
-    TARGET => 'libp7.so.0.3.3',
+    TARGET => 'libp7.so.0.4.0',
 );
 
 @Servbuild::Makemaker::C::sources = (
