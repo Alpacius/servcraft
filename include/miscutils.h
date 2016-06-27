c998d59ed9e426860651a2732025b1dee1172df3
Akvelog <sceneviper@hotmail.com>
2016-06-27 22:37:02 +0900

    Fix: exact evaluation on macro parameter ptr.

--- include/miscutils.h
+++ include/miscutils.h
@@ -44,7 +44,7 @@
 
 #define    offset_of(type, member) ((size_t) &((type *) 0)->member)
 #define    container_of(ptr, type, member) ({ \
-     const typeof(((type *) 0)->member) *__mptr = ptr; \
+     const typeof(((type *) 0)->member) *__mptr = (ptr); \
      (type *) ((char *) __mptr - offset_of(type, member)); })
 
 #endif      // MISCUTILS_H_
